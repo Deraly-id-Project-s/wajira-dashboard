647eace4c7cd0d9978b5813cc02a7fdf60917da4
hawkxc068 <hawkxc068@gmail.com>
2025-10-23 13:43:05 +0700

    update: update function helper

--- app/Helpers/function.php
+++ app/Helpers/function.php
@@ -14,27 +14,34 @@ if (!function_exists('pageClickTracker')) {
 
             return true;
         } catch (\Exception $err) {
-            return false;   
+            return false;
         }
     }
 }
 
 if (!function_exists('pageVisitor')) {
-    function pageVisitor(string $session_id, string $ip_address, string $user_agent, string $platform, string $url_visited = '/', string $referrer, string $country): bool
-    {
+    function pageVisitor(
+        string $session_id,
+        string $ip_address,
+        string $user_agent,
+        string $platform,
+        string $country,
+        string $url_visited = '/',
+        ?string $referrer = null
+    ): bool {
         $visitor = PageVisitor::CheckHost($ip_address, $session_id)->last_accessed_at ?? null;
-        
+
         try {
             $visitor_data = new PageVisitor;
-            $visitor_data->session_id = $session_id ?? null;
-            $visitor_data->ip_address = $ip_address ?? null;
-            $visitor_data->user_agent = $user_agent ?? null;
-            $visitor_data->platform = $platform ?? null;
-            $visitor_data->url_visited = $url_visited ?? null;
-            $visitor_data->referrer = $referrer ?? null;
-            $visitor_data->country = $country ?? null;
+            $visitor_data->session_id = $session_id;
+            $visitor_data->ip_address = $ip_address;
+            $visitor_data->user_agent = $user_agent;
+            $visitor_data->platform = $platform;
+            $visitor_data->url_visited = $url_visited;
+            $visitor_data->referrer = $referrer;
+            $visitor_data->country = $country;
             $visitor_data->last_accessed_at = $visitor ?? now();
-            
+
             $visitor_data->save();
         } catch (\Exception $err) {
             Log::info('Page Visitor Error: ' . $err->getMessage());
