87cf060064eb6c30ad490c9066c2ab5c527ac90f
hawkxc068 <hawkxc068@gmail.com>
2025-11-06 15:19:00 +0700

    update: update laravel inertia prerender optimization

--- app/Http/Middleware/PrerenderIfCrawler.php
+++ app/Http/Middleware/PrerenderIfCrawler.php
@@ -4,17 +4,29 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class PrerenderIfCrawler
 {
     public function handle(Request $request, Closure $next)
     {
         $userAgent = $request->header('User-Agent');
-        $isCrawler = preg_match('/bot|crawler|spider|slurp|facebookexternalhit/i', $userAgent);
+        $isCrawler = preg_match('/bot|crawler|spider|slurp|facebookexternalhit|bingbot|googlebot/i', $userAgent);
 
-        if ($request->isMethod('GET') && $isCrawler) {
-            $url = 'https://service.prerender.io/' . $request->fullUrl();
-            return redirect()->away($url);
+        if ($request->isMethod('GET') && !$request->is('api/*') && $isCrawler) {
+            try {
+                $client = new Client();
+                $response = $client->get('https://service.prerender.io/' . $request->fullUrl(), [
+                    'headers' => [
+                        'X-Prerender-Token' => env('PRERENDER_TOKEN'),
+                    ]
+                ]);
+
+                return response($response->getBody(), $response->getStatusCode())
+                    ->header('Content-Type', 'text/html');
+            } catch (\Exception $e) {
+                return $next($request);
+            }
         }
 
         return $next($request);
